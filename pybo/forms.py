76b20adb884593c1bdeba5879da8fa813f78dfb8
JoGangMin <er1458@naver.com>
2020-09-08 19:27:11 +0900

    ~ 02-10 fix error

--- pybo/forms.py
+++ pybo/forms.py
@@ -6,3 +6,8 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['subject', 'content']
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
